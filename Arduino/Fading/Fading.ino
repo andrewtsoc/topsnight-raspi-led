5c5f322b08c01e3ac622af57004b12d35ceb25f0
Andrew Tan <andrew@atsoc.(none)>
2013-06-18 13:36:33 -0400

    Fade multiple pins at a time

--- Arduino/Fading/Fading.ino
+++ Arduino/Fading/Fading.ino
@@ -1,31 +1,48 @@
+//adjust these values as needed
 const int numPins = 6;
-const int fadeTime = 1000;
-const int minWait = 1000;
-const int rangeWait = 2000;
+const int minFadeTime = 500;
+const int rangeFadeTime = 500;
+const int minWait = 500;
+const int rangeWait = 500;
+const int seed = 1337;
+
 int pins[numPins] = {3,5,6,9,10,11};
 boolean isOn[numPins];
+boolean isBeingChanged[numPins];
+
 void setup()  { 
- srand(1337);
- for(int i=0;i<numPins;i++)
+ srand(seed);
+ for(int i=0;i<numPins;i++){
    isOn[i]=rand()%2;
-} 
-void switchState(int pin){
-  pin %= numPins;
-  if (!isOn[pin])
-    for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=5) { 
-      analogWrite(pins[pin], fadeValue);          
-      delay(fadeTime/256);                            
-    } 
-  else
-    for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=5) { 
-      analogWrite(pins[pin], fadeValue);             
-      delay(fadeTime/256);                            
-    } 
-  isOn[pin] = !isOn[pin];
+   analogWrite(pins[i],(isOn[i])?255:0); //initialize states
+ }
+ delay(3000);
 }
+
+void switchStates(){
+  int delayTime = (rand() % rangeFadeTime + minFadeTime)/51;
+  for(int fadeValue = 0 ; fadeValue < 256; fadeValue +=5) { //fade
+    for(int i=0;i<numPins;i++){ //loop over all pins
+        if (isBeingChanged[i]){ //change only marked pins
+          int pin = pins[i];
+          analogWrite(pin, (isOn[i])? 255-fadeValue : fadeValue); //fade on or off 
+        }
+    }
+    delay(delayTime); 
+  }
+  for(int i=0;i<numPins;i++)
+    isOn[i] = !isBeingChanged[i] != !isOn[i]; //xor
+}
+
+void markChanges(){
+  for(int i=0;i<numPins;i++)
+    isBeingChanged[i] = (rand()%4 == 0); 
+}
+
 void loop()  {
-  switchState(rand());
-  delay(rand()%rangeWait + minWait);
+  markChanges();
+  switchStates();
+  delay(rand()%rangeWait + minWait); //delay between changes
 }
 
 
